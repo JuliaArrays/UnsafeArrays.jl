53e8df54c6a3babedd2a76c7a67eac68e577afa5
Sergey Fokin <sergey.fokin@graystonetrading.com>
2022-07-01 17:58:34 +0200

    Optimize conversion with no size change

--- src/unsafe_array.jl
+++ src/unsafe_array.jl
@@ -149,12 +149,16 @@ end
 Base.deepcopy(A::UnsafeArray) = copyto!(similar(A), A)
 
 function Base.reinterpret(::Type{DST}, A::UnsafeArray{SRC}) where {DST, SRC}
-    sz = size(A)
-    sz1, rem = divrem(sz[1] * sizeof(SRC), sizeof(DST))
-    @boundscheck if rem != zero(rem)
-        throw(ArgumentError("Resulting array would have non-integral first dimension"))
+    if sizeof(DST) != sizeof(SRC)
+        sz = size(A)
+        sz1, rem = divrem(sz[1] * sizeof(SRC), sizeof(DST))
+        @boundscheck if rem != zero(rem)
+            throw(ArgumentError("Resulting array would have non-integral first dimension"))
+        end
+        UnsafeArray(convert(Ptr{DST}, pointer(A)), (sz1, Base.tail(sz)...))
+    else
+        UnsafeArray(convert(Ptr{DST}, pointer(A)), size(A))
     end
-    UnsafeArray(convert(Ptr{DST}, pointer(A)), (sz1, Base.tail(sz)...))
 end
 
 # # Defining Base.unaliascopy results in very bad broadcast performance for
